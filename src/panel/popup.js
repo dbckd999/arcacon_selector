627388ae1e4bb419dca8d293ba2215f97b86f333
dbckd999 <dbckd999@naver.com>
2025-12-12 05:20:35 +0900

    chor: 미포함 라이브러리 추가

--- src/panel/popup.js
+++ src/panel/popup.js
@@ -7,6 +7,7 @@ import '@shoelace-style/shoelace/dist/components/divider/divider.js';
 import '@shoelace-style/shoelace/dist/components/range/range.js';
 import '@shoelace-style/shoelace/dist/components/tab-panel/tab-panel.js';
 
+import '@shoelace-style/shoelace/dist/components/tag/tag.js';
 import '@shoelace-style/shoelace/dist/components/dialog/dialog.js';
 import '@shoelace-style/shoelace/dist/components/input/input.js';
 import '@shoelace-style/shoelace/dist/components/tab/tab.js';
