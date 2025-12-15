fbaab4614923f143e355d0bcb8befe44716c264a
dbckd999 <dbckd999@naver.com>
2025-12-16 04:13:17 +0900

    build: 빌드 편의성 개선

--- pack.js
+++ pack.js
@@ -3,15 +3,22 @@ const { parse, resolve } = require('path');
 const AdmZip = require('adm-zip');
 
 try {
+
+  const target = process.argv.find(v => v.startsWith('--target='))?.split('=')[1];
+  if (!target) throw new Error('target required');
+
+  console.log(resolve(__dirname, target, 'build', 'manifest.json'));
   const { base } = parse(__dirname);
   const { version } = JSON.parse(
-    readFileSync(resolve(__dirname, 'build', 'manifest.json'), 'utf8')
+    readFileSync(resolve(__dirname, target, 'build', 'manifest.json'), 'utf8')
   );
 
   const outdir = 'release';
-  const filename = `${base}-v${version}.zip`;
+  const filename = `${base}-${target}-v${version}.zip`;
   const zip = new AdmZip();
-  zip.addLocalFolder('build');
+
+  const buildDir = resolve(__dirname, target, 'build');
+  zip.addLocalFolder(buildDir);
   if (!existsSync(outdir)) {
     mkdirSync(outdir);
   }
@@ -21,5 +28,6 @@ try {
     `Success! Created a ${filename} file under ${outdir} directory. You can upload this file to web store.`
   );
 } catch (e) {
+  console.error(e);
   console.error('Error! Failed to generate a zip file.');
 }
